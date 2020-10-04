2e42775e1344c73f5c7c8e098addce35b9923b2c
Chris Dominy <cdominy@utexas.edu>
2020-10-04 16:37:07 -0500

    Added delete api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,43 @@
-const db = require("../db/db");
+const db = require("../db/db.json");
+const fs = require("fs");
+let id = db.length + 1;
 
 module.exports = function (app) {
-    app.get("/api/notes", function(req, res){
-        res.json(db);
-    });
+  app.get("/api/notes", function (req, res) {
+    res.json(db);
+  });
 
-    app.post("/api/notes", function(req, res){
-        db.push(req.body);
-        res.json(true);
+  app.post("/api/notes", function (req, res) {
+    req.body.id = ++id;
+    for (let i = 0; i < db.length; i++) {
+      console.log(db[i].id, id);
+      if (db[i].id === id) {
+        req.body.id = id++;
+      }
+    }
+    db.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(db), function (err) {
+      if (err) {
+        throw err;
+      }
+      res.json(db);
     });
+  });
 
-    app.delete("api/notes/:id", function(req, res){
-        
-    })
-}
+  app.delete("/api/notes/:id", function (req, res) {
+    console.log(req.params.id);
+    const rowID = parseInt(req.params.id);
+    console.log(rowID);
+    for (let i = 0; i < db.length; i++) {
+      if (db[i].id === rowID) {
+        db.splice(i, 1);
+      }
+    }
+    fs.writeFile("./db/db.json", JSON.stringify(db), function (err) {
+      if (err) {
+        throw err;
+      }
+      res.json(db);
+    });
+  });
+};
